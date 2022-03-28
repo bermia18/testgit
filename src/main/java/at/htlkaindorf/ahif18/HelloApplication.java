c854ae211a2e1b0995bda74bb0515b63b44dfe46
MichaelMaierJosef <maimia18@htl-kaindorf.at>
2022-03-28 14:10:55 +0200

    ArtifactCheck

--- src/main/java/at/htlkaindorf/ahif18/HelloApplication.java
+++
@@ -1,9 +0,0 @@
-package at.htlkaindorf.ahif18;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class HelloApplication extends Application {
-
-}
